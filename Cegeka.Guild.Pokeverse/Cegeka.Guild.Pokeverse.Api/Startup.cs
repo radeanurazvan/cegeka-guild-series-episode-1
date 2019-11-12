5fe2b86e652936e3c303f166b9a819cae346eb9b
Razvan Radeanu <razvan.radeanu@cegeka.com>
2019-11-12 17:29:06 +0200

    Implemented in memory persistence

--- Cegeka.Guild.Pokeverse/Cegeka.Guild.Pokeverse.Api/Startup.cs
+++ Cegeka.Guild.Pokeverse/Cegeka.Guild.Pokeverse.Api/Startup.cs
@@ -1,3 +1,4 @@
+using Cegeka.Guild.Pokeverse.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,7 +19,9 @@ namespace Cegeka.Guild.Pokeverse.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddPersistence()
+                .AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
